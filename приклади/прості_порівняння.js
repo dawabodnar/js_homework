52e7e67724a23e0fbe499b0059c21032b0436a2b
Illia Trach <illyatrach@gmail.com>
2020-02-29 21:44:05 +0200

    Додав прості порівняння

---
+++ приклади/прості_порівняння.js
@@ -0,0 +1,13 @@
+console.log(1 > 2); //false
+console.log(1 < 2); //true
+console.log(2 * 2); // 4
+console.log(2 == 2); // true
+console.log(2 == 3); //false
+console.log(2 != 3); // true
+console.log("hello"); // hello
+console.log("hello " + "world"); // hello world
+console.log("Сьогодні " + 8); // Сьогодні 8
+var a = "slovo";
+console.log("raz " + a); // raz slovo
+console.log("g" == "g"); // true
+console.log(4.5 == 4.50); // true
